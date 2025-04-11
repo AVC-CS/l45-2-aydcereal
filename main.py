7cef0624dd2e2a43f350fc4cf68ed5b176fe4be1
aydcereal <jordyfigueroa93@icloud.com>
2025-04-10 17:17:27 -0700

    final commit

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    while total < 100:
+        num = random.randint(1,10)
+        print(num)
+        numbers.append(num)   
+        total += num
+   
+    total = total - numbers[-1]
+    
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
